54c8fb339319a2b3e460204781caddb3eabb9c26
nerioalfonso <nerioalfonso@nerio-PC.Home>
2021-08-24 19:39:52 -0400

    ajustada dirección de ep

--- src/api.ts
+++ src/api.ts
@@ -2,5 +2,5 @@ require('dotenv').config();
 import express =require("express");
 let api : express.Application =express();
 let port =process.env.PORT;
-api.get('/', (req, res) => res.send('¡Holas mundo!'));
+api.get('/api', (req, res) => res.send('¡Holas mundo!'));
 api.listen(port, () => console.log(`Arriba, puerto ${port}`));
