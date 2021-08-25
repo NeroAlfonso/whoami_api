3640b585611bc31c3a7e6331c233eb01393fdaef
nerioalfonso <nerioalfonso@nerio-PC.Home>
2021-08-25 09:01:09 -0400

    update

--- src/api.ts
+++ src/api.ts
@@ -3,4 +3,5 @@ import express =require("express");
 let api : express.Application =express();
 let port =process.env.PORT;
 api.get('/api', (req, res) => res.send('¡Holas mundo!'));
+api.get('/', (req, res) => res.send('Pantalla de inicio'));
 api.listen(port, () => console.log(`Arriba, puerto ${port}`));
